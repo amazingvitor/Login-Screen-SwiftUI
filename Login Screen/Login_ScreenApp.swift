cf0d19287320c1fe3ba2ffc9330b945450b439cb
Vitor Hugo <vitor.h8@icloud.com>
2021-05-15 17:20:30 -0300

    Initial Commit

---
+++ Login Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by Vitor Hugo on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
